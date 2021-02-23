a3fbba10d214d6a79db05b575f036a722c4eac2e
Alexander Dean <agdean@ncsu.edu>
2021-02-23 09:45:42 -0500

    Tweaks for performance evaluation.

--- BES/GPIO-Out/CPP_sysfs/simple_out.cpp
+++ BES/GPIO-Out/CPP_sysfs/simple_out.cpp
@@ -1,6 +1,11 @@
 /* A Simple GPIO application
 *  Written by Derek Molloy for the book "Exploring Raspberry Pi" */
 
+/* ECE 785 Modifications:
+   Infinite loop to generate bursts of pulses, alternating between
+   setValue and streamWrite methods.
+*/
+
 #include<iostream>
 #include<unistd.h>  // for the usleep() function
 #include"GPIO.h"
@@ -8,18 +13,17 @@ using namespace exploringRPi;
 using namespace std;
 
 int main(){
-   GPIO outGPIO(17), inGPIO(27);    // pin 11 and pin 13
-
+   GPIO outGPIO(17);    // pin 11
 
    while (1) {
 	   outGPIO.setDirection(OUTPUT);    // basic output example
-	   for (int i=0; i<100; i++){        // flash the LED 
+	   for (long i=0; i<10000; i++){        // flash the LED 
 	      outGPIO.setValue(HIGH);       // turn the LED on
 	      outGPIO.setValue(LOW);        // turn the LED off
 	   }
 
 	   outGPIO.streamOpen();            // fast write example
-	   for (int i=0; i<1000; i++){   // write
+	   for (long i=0; i<500000; i++){   // flash the LED
 	      outGPIO.streamWrite(HIGH);    // high
 	      outGPIO.streamWrite(LOW);     // immediately low, repeat
 	   }
